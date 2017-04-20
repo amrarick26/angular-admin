d5b5fff3c95d9e1f83dea8e8625af9e7c8432ea8
Alexa Rarick <ararick@four51.com>
2017-04-20 11:12:03 -0500

    Fix for ID error message

--- src/app/orderManagement/orders/js/orders.config.js
+++ src/app/orderManagement/orders/js/orders.config.js
@@ -20,33 +20,12 @@ function OrdersConfig($stateProvider) {
                 OrderList: function(ocOrdersService, Parameters) {
                     return ocOrdersService.List(Parameters);
                 },
-<<<<<<< HEAD
-                BuyerCompanies: function(OrderCloud) {
-                    return OrderCloud.Buyers.List(null, 1, 100);
-                },
-                UserGroupsList: function($q, OrderCloud, Parameters, BuyerCompanies) {
-                    var queue = [];
-                    _.each(BuyerCompanies.Items, function(buyer) {
-                        queue.push(function(){
-                            return OrderCloud.UserGroups.List(null, null, 100, null, null, null, buyer.ID)
-                                .then(function(data) {
-                                    return data.Items;
-                                });
-                        }());
-                    });
-                    return $q.all(queue)
-                        .then(function(results) {
-                            var userGroups = [].concat.apply([], results);
-                            return userGroups;
-                        })
-=======
                 BuyerCompanies: function(OrderCloudSDK) {
                     var options = {
                         page: 1,
                         pageSize: 100
                     };
                     return OrderCloudSDK.Buyers.List(options);
->>>>>>> ac68fad1f47bd2f7eff5d66c96ba2dfbd9e8a5ac
                 }
             }
         })
